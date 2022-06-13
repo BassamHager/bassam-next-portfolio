d950c09ca1c5cd16a81c352f59d4adda68a5deba
BassamHajar <b2dndr1@gmail.com>
2022-06-13 20:11:14 +0200

    updated ProjectCard.jsx styling

--- components/molecules/projects/ProjectCard.jsx
+++ components/molecules/projects/ProjectCard.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import Image from "next/image";
 
 const ProjectCard = ({ imgURL, projectTitle, projectDesc }) => {
+  const justifyDescription = (text) =>
+    text?.split("").splice(0, 150).join("") + "...";
+
   return (
-    <div className="project-card hover:shadow-xl hover:scale-105 transition-all ease-in-out duration-300 m-4 w-1/3 cursor-pointer text-center">
+    <div className="project-card shadow-sm hover:shadow-xl hover:scale-105 ease-in duration-300 m-4 w-1/3 cursor-pointer text-center hover:bg-gradient-to-r from-[blue] to-[purple] h-full hover:text-[#ccc] rounded-md max-w-xs bg-gray-500 min-h-[400px] min-w-[400px] p-4 project-title-hover">
       <div className="image-wrapper grid ">
-        <Image src={imgURL} alt="project image" height="300" width="600" />
+        <Image
+          src={imgURL}
+          alt="project image"
+          height="300"
+          width="600"
+          // layout="fill"
+          objectFit="contain"
+          className="rounded-md min-w-[500px]"
+        />
       </div>
       <div className="project-card-body">
         <h4 className="">{projectTitle}</h4>
-        <p>{projectDesc}</p>
+        <p className="text-justify p-3 ">{justifyDescription(projectDesc)}</p>
       </div>
     </div>
   );
--- components/molecules/projects/Projects.jsx
+++ components/molecules/projects/Projects.jsx
@@ -12,32 +12,54 @@ const Projects = () => {
           <ProjectCard
             imgURL="/../public/assets/images/projects/abn_amro_tech_assignment.jpg"
             projectTitle="abn_amro_tech_assignment"
-            projectDesc="..."
+            projectDesc="Simple React.js app, using tvmaze API, the state managed using React Context API, this app lets the user views a few lists of TV shows based on different genres. Each list has a few popular TV shows based on their rating. Clicking a TV show card shall display a new screen with the details of the clicked show."
           />
           <ProjectCard
             imgURL="/../public/assets/images/projects/dev_connector_19.jpg"
             projectTitle="dev_connector_19"
-            projectDesc="..."
+            projectDesc="HackYourSocial is a full-stack social network web application built with the MERN stack. :bowtie:
+            Please click to view HackYourSocial ✌️
+            HackYourSocial allows developers to interact with each other by creating profiles showing their skills, education, experiences and enables others to get accesses to their other social media and github, also enables them to share posts, comments, likes and other types of content. 👍"
           />
           <ProjectCard
             imgURL="/../public/assets/images/projects/EthSwap.jpg"
             projectTitle="EthSwap"
-            projectDesc="..."
+            projectDesc="Is a Cryptocurrency Exchange app for Blockchain - PT 1
+            DEPENDENCIES
+            * Node.js: https://nodejs.org/en/
+            * Ganache: https://www.trufflesuite.com/ganache
+            * Truffle: https://www.trufflesuite.com/
+            * Metamask:
+            RESOURCES
+            * Starter Kit: https://github.com/dappuniversity/sta...
+            * ERC-20 Standard: https://github.com/ethereum/EIPs/blob..."
           />
           <ProjectCard
             imgURL="/../public/assets/images/projects/tv-show-finder.jpg"
             projectTitle="tv-show-finder"
-            projectDesc="..."
+            projectDesc="It's a responsive web app, built with the following Js-stack:
+            - API: REST
+            - Frontend: Vue.js scaffolded with Vite
+            - State Management Solution: Pinia
+            - Style: SCSS-Tailwinds
+            - Routing: Vue-router"
           />
           <ProjectCard
             imgURL="/../public/assets/images/projects/college_football_mws_ta.jpg"
             projectTitle="college_football_mws_ta"
-            projectDesc="..."
+            projectDesc="This project was bootstrapped with Create React App. It starts in a login page which makes use of a 6 digit code (numeral password) to enter the application. Then, homepage displays a grid of clickable cards (school-teams). Each card has the school team name & its logo. Clicking a card should route to a new screen (teamDetails) with the details of the clicked team where user can see (TeamDetails) the upcoming & previous games of the clicked team."
           />
           <ProjectCard
             imgURL="/../public/assets/images/projects/max_place_sharer.jpg"
             projectTitle="max_place_sharer"
-            projectDesc="..."
+            projectDesc="It's a responsive web app, built with the following Js-stack:
+            Backend: Node
+            API: REST
+            Frontend: React (hooks)
+            State Management Solution: Context-api
+            Style: css3
+            Forms: customized hook
+            Routing: React-router"
           />
         </div>
       </div>
