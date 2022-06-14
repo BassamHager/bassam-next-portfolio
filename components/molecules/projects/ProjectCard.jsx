c0a06337538b3d7009f2a5a087d8807f5aee7551
BassamHajar <b2dndr1@gmail.com>
2022-06-14 09:06:21 +0200

    wip - updated minor texts

--- components/molecules/projects/ProjectCard.jsx
+++ components/molecules/projects/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = ({ imgURL, projectTitle, projectDesc }) => {
     text?.split("").splice(0, 150).join("") + "...";
 
   return (
-    <div className="project-card shadow-sm hover:shadow-xl hover:scale-105 ease-in duration-300 m-4 w-1/3 cursor-pointer text-center hover:bg-gradient-to-r from-[blue] to-[purple] h-full hover:text-[#ccc] rounded-md max-w-xs bg-gray-500 min-h-[400px] min-w-[400px] p-4 project-title-hover">
+    <div className="project-card shadow-sm hover:shadow-xl hover:scale-105 ease-in duration-300 m-4 w-1/3 cursor-pointer text-center hover:bg-gradient-to-r from-[maroon] to-[black] h-full hover:text-[#ccc] rounded-md max-w-xs bg-gray-500 min-h-[400px] min-w-[400px] p-4 project-title-hover">
       <div className="image-wrapper grid ">
         <Image
           src={imgURL}
--- components/molecules/projects/Projects.jsx
+++ components/molecules/projects/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
           />
           <ProjectCard
             imgURL="/../public/assets/images/projects/dev_connector_19.jpg"
-            projectTitle="dev_connector_19"
+            projectTitle="HackYourSocial"
             projectDesc="HackYourSocial is a full-stack social network web application built with the MERN stack. :bowtie:
             Please click to view HackYourSocial ✌️
             HackYourSocial allows developers to interact with each other by creating profiles showing their skills, education, experiences and enables others to get accesses to their other social media and github, also enables them to share posts, comments, likes and other types of content. 👍"
